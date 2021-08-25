4f7a3f173832c5bc2efeb8868aba3ec764e4e3fd
kimkh0930 <kimkh940930@gmail.com>
2021-08-25 17:06:25 +0900

    feat: create TAkeTransTest.java
    main

---
+++ java/ObjectCollaboration/TakeTransTest.java
@@ -0,0 +1,31 @@
+package oop.ch14;
+
+public class TakeTransTest {
+
+    public static void main(String[] args) {
+        Student studentJ = new Student("James",5000);
+        Student studentT = new Student("Tomas",10000);
+        Student studentE = new Student("Edward",20000);
+
+        Bus bus100 = new Bus(100);
+        Bus bus500 = new Bus(500);
+
+        studentJ.takeBus(bus100);
+
+        Subway greenSubway = new Subway(2);
+        studentT.takeSubway(greenSubway);
+
+        Taxi taxi1 = new Taxi("잘 나간다 운수");
+        studentE.takeTaxi(taxi1);
+
+        studentJ.showInfo();
+        studentT.showInfo();
+        studentE.showInfo();
+
+        bus100.showBusinfo();
+        bus500.showBusinfo();
+        greenSubway.showSubwayinfo();
+        taxi1.showTaxiinfo();
+
+    }
+}
