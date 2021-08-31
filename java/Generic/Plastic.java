8abedc39a2bedce3231da8a99d6914f5a411e9ec
kimkh0930 <kimkh940930@gmail.com>
2021-08-31 17:11:35 +0900

    feat: Plastic.java Powder.java
    Sub class to practice Generic T extends with java

---
+++ java/Generic/Plastic.java
@@ -0,0 +1,12 @@
+package Chapter5.ch07;
+
+public class Plastic extends Material{
+    public String toString(){
+        return "재료는 Plastic 입니다.";
+    }
+
+    @Override
+    public void doPrinting() {
+
+    }
+}
