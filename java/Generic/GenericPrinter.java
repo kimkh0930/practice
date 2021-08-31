988aa68145e1ed7c9302e626018a3654d981df46
kimkh0930 <kimkh940930@gmail.com>
2021-08-31 17:12:35 +0900

    feat: create GenericPrinter/Test.java
    Generic and test file

---
+++ java/Generic/GenericPrinter.java
@@ -0,0 +1,18 @@
+package Chapter5.ch07;
+
+public class GenericPrinter<T extends Material> {
+
+    private T material;
+
+    public T getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(T material) {
+        this.material = material;
+    }
+
+    public String toString(){
+        return material.toString();
+    }
+}
